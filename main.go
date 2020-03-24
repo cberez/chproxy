8ab70e600845f0ec35a4bc76a78f66fe1668134e
Cesar Berezowski <cesar@adaltas.com>
2020-03-24 18:23:21 +0100

    main refactoring

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/cberez/chproxy/proxy"
+	"log"
 	"strings"
+
+	"github.com/cberez/chproxy/proxy"
 )
 
 func main() {
-	addresses := "localhost:9222"
-	port := "8080"
 
-	p := chproxy.Proxy{ApiKey: "key", Timeout: 5, Addresses: strings.Split(addresses, ",")}
+	var addresses string
+	flag.StringVar(&addresses, "addresses", "localhost:9222", "comma separated list of chrome headless addresses")
+
+	var port string
+	flag.StringVar(&port, "port", "8080", "port to listen to for incoming connections")
+
+	var timeout int
+	flag.IntVar(&timeout, "timeouyt", 5, "connection timeout in seconds")
+
+	var apiKey string
+	flag.StringVar(&apiKey, "apikey", "key", "api key to use")
+
+	flag.Parse()
+
+	p := chproxy.Proxy{ApiKey: apiKey, Timeout: timeout, Addresses: strings.Split(addresses, ",")}
+
+	log.Printf("listening on port %s", port)
+
 	p.ServeAndHandle(fmt.Sprintf("localhost:%s", port))
 }
